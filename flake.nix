83381222a45e82d52a043be63935dcea225aba99
Luca Leon Happel <lucahappel99@gmx.de>
2023-01-21 21:23:28 +0100

    add: documentation about using flakes

--- flake.nix
+++ flake.nix
@@ -15,6 +15,31 @@
         pkgs = import nixpkgs { inherit system; };
       in
         rec {
+          # This section is here, so users can run:
+          # > nix run github:Quoteme/nixos-godot-bin
+          # and start Godot without any further configuration.
+          #
+          # > nix run github:Quoteme/nixos-godot-bin\#godot
+          # > nix run github:Quoteme/nixos-godot-bin\#godotHeadless
+          # > nix run github:Quoteme/nixos-godot-bin\#godotMono
+          # are also available.
+          defaultApp = apps.godot;
+          apps.godot = {
+            type = "app";
+            program = "${packages.godot}/bin/godot";
+          };
+          apps.godotHeadless = {
+            type = "app";
+            program = "${packages.godot}/bin/godot-headless";
+          };
+          apps.godotMono = {
+            type = "app";
+            program = "${packages.godotMono}/bin/godot-mono";
+          };
+
+          # This is for installing Godot using flakes
+          # just add this flake to your flakes inputs, and then you can
+          # use these packages to include Godot in your environment.
           defaultPackage = packages.godot;
           packages.godot = pkgs.callPackage ./pkgs/godot { };
           packages.godotHeadless = pkgs.callPackage ./pkgs/godot/headless.nix {
